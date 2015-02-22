878af02292e3f6b09acc25e14a9009a1bfaad136
Michael Catanzaro <mcatanzaro@gnome.org>
2015-02-22 10:25:33 -0600

    Rename the library and reorganize the repository
    Notably, switch to nonrecursive automake, because I don't know how to
    make Vala produce a single header and vapi otherwise.

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+srcdir=`dirname $0`
+[ -z "$srcdir" ] && srcdir=.
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME git (or from"
-    echo "your OS vendor's package manager)."
-    exit 1
-}
-
-# This blurb from systemd, LGPL v2.1+
-if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then
-        # This part is allowed to fail
-        cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
-        chmod +x .git/hooks/pre-commit && \
-        echo "Activated pre-commit hook." || :
+if [ ! -f "$srcdir/configure.ac" ]; then
+ echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
+ exit 1
 fi
 
-. gnome-autogen.sh
+which gnome-autogen.sh || {
+	echo "You need to install gnome-common from GNOME Git"
+	exit 1
+}
+
+. gnome-autogen.sh "$@"
