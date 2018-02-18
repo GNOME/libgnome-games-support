GNOME/libgnome-games-support

commit b42a2515acf70dc865908d9ef29c4a1af00e166e

Prefer GObject-style construction

games/scores/score.vala
@@ -24,18 +24,27 @@ namespace Scores {
 public class Score : Object
 {
     public long score { get; set; }
-    public string user { get; set; }
 
     /* Although the scores dialog does not currently display the time a
      * score was achieved, it did in the past and it might again in the future.
      */
-    public int64 time { get; set; }
+    private int64 _time;
+    public int64 time
+    {
+        get { return _time; }
+        set { _time = (value == 0 ? new DateTime.now_local ().to_unix () : value); }
+    }
+
+    private string _user;
+    public string user
+    {
+        get { return _user; }
+        set { _user = (value == null ? Environment.get_real_name () : user); }
+    }
 
     public Score (long score, int64 time = 0, string? user = null)
     {
-        this.score = score;
-        this.time = (time == 0 ? new DateTime.now_local ().to_unix () : time);
-        this.user = (user == null ? Environment.get_real_name () : user);
+        Object (score: score, time: time, user: user);
     }
 
     public static bool equals (Score a, Score b)

games/gridframe.vala
@@ -114,7 +114,7 @@ public class GridFrame : Gtk.Bin
 
     public GridFrame (int width, int height)
     {
-        @set (width, height);
+        Object (width: width, height: height);
     }
 
     public new void @set (int width, int height)

games/scores/category.vala
@@ -45,8 +45,7 @@ public class Category : Object
 
     public Category (string key, string name)
     {
-        this.key = key;
-        this.name = name;
+        Object (key: key, name: name);
     }
 }
 

commit cbf267e4cbc794fd5cd2bb7c005803fe46645dff

scores: Fix Score user property
I noticed that, after changing your name on the high score dialog, the
score was saved with the original name instead of the new name. After
way too much debugging, it turned out to be a typo here. It regressed in
b42a2515acf70dc865908d9ef29c4a1af00e166e.

games/scores/score.vala
@@ -39,7 +39,7 @@ public class Score : Object
     public string user
     {
         get { return _user; }
-        set { _user = (value == null ? Environment.get_real_name () : user); }
+        set { _user = (value == null ? Environment.get_real_name () : value); }
     }
 
     public Score (long score, int64 time = 0, string? user = null)